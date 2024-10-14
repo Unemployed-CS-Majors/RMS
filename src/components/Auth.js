0e5a008b1d05e01bcdaf62ffb816ad05cb1b83d3
Nathan Lam <nathanl1605@gmail.com>
2024-10-14 20:27:13 +0100

    Layout for login and registration

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,86 @@
 import React from "react";
+import { useState } from "react";
 import "../Styles/Auth.css";
 
 export default function Auth(){
-    return(
-        <div>
+    const [loginForm, setLoginForm] = useState(true);
+    const [registerForm, setRegisterForm] = useState(false);
 
+    const showLogin = () => {
+        setLoginForm(true);
+        setRegisterForm(false);
+    }
+
+    const showRegister = () => {
+        setLoginForm(false);
+        setRegisterForm(true);
+    }
+
+    return(
+        <div className="auth-container">
+            <div className="content-container">
+                <div className="btn-container">
+                    <button className="login-btn auth-btn" onClick={showLogin}>Login</button>
+                    <button className="register-btn auth-btn" onClick={showRegister}>Register</button>
+                </div>
+                <div className="form-container" style={{width: '100%'}}>
+                    {loginForm ? (
+                        <form style={{display: 'flex', flexDirection: 'column'}}>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Email"
+                                required
+                            />
+
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                required
+                            />
+                            <button type="submit" className="submit-btn">Login</button>
+                        </form>
+                    ) : (
+                        <form style={{display: 'flex', flexDirection: 'column'}}>
+                            <div className="name-container" style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <input
+                                    type="text"
+                                    id="firstName"
+                                    placeholder="First Name"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    id="lastName"
+                                    placeholder="Last Name"
+                                    required
+                                />
+                            </div>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Enter Email"
+                                required
+                            />
+
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Enter Password"
+                                required
+                            />
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                placeholder="Confirm Password"
+                                required
+                            />
+                            <button type="submit" className="submit-btn">Login</button>
+                        </form>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "../Styles/Main.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Main(){
+    const navigate = useNavigate(); 
+
+    const handleClick = () => {
+        navigate("/auth"); 
+    };
+
     return(
+        /* Write your HTML Code here */
         <div>
+            <button onClick={handleClick} style={{padding: "1rem"}}>Sign In</button>
         </div>
-    )
+    );
 }
