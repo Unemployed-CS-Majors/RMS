a3460a4813012c0af98cb8eb1957b4cca2a1ee13
Roksana Olech <roksana.olech3@gmail.com>
2024-10-17 15:22:36 +0100

    draft of the main page
    changed the position of the button and added some features,
    draft of a logo?
    added some colours (might change)

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,14 +5,17 @@ import { useNavigate } from "react-router-dom";
 export default function Main(){
     const navigate = useNavigate(); 
 
-    const handleClick = () => {
+    const handleNavigateToAuth = () => {
         navigate("/auth"); 
     };
 
     return(
         /* Write your HTML Code here */
+    
         <div>
-            <button onClick={handleClick} style={{padding: "1rem"}}>Sign In</button>
+            <button className="signInBtn" onClick={handleNavigateToAuth} style={{padding: "1rem"}}>Sign In</button>
+            <h1 className="rmsLogo">RMS</h1>
+            <h2 className="rmsLogo2">Restaurant Management System</h2>
         </div>
     );
 }
